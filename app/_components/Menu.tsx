631d14eedb99b530cdeb48bc7d1cf6d1264f372a
Ridit <riditjangra09@gmail.com>
2026-05-31 19:19:57 +0530

    feat: new test game button

--- app/_components/Menu.tsx
+++ app/_components/Menu.tsx
@@ -1,16 +1,17 @@
 export function Menu() {
   return (
-    <div className="flex items-center justify-between fixed z-1000 w-full px-8">
+    <div className="flex items-center justify-between fixed z-1000 w-full">
       <a href="https://hackclub.com" target="_blank">
         <img src="/hc-logo.png" alt="logo" className="w-50" />
       </a>
       <a
-        href="https://game.pixl.rsvp"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="px-6 py-2 bg-[#ec3750] text-white rounded-lg font-pixel hover:shadow-lg hover:-translate-y-1 transition-all"
+        // onClick={handleRSVP}
+        className="text-center px-5 mr-2 py-2 text-base sm:text-xl md:text-2xl hover:shadow-2xl shadow-[#ff8c37] bg-[#ff8c37] cursor-pointer text-white hover:-translate-y-1 hover:-translate-x-1 border-black border-r-8 border-t-2 border-l-2 hover:border-b-12 border-b-8 transition-all"
+        // whileHover={{ scale: 1.03 }}
+        // whileTap={{ scale: 0.97 }}
+        href="https://play.pixl.rsvp"
       >
-        Test the game
+        Test the game!
       </a>
     </div>
   );
